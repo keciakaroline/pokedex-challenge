79d5baa97ddf7fd2b5bd0a735279bbe8d8282b33
Kécia Santos <keciakaroline@gmail.com>
2022-07-02 19:32:46 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+import "./styles/Navbar.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="fixed">
+      <Toolbar className="AppBar">
+        <Link to="/" className="Link">
+          <Typography variant="h6" component="div" className="Typography">
+            Pokedex
+          </Typography>
+        </Link>
+        <Link to="/favorites" className="Link">
+          <Typography variant="h6" component="div" className="Typography">
+            Favorites
+          </Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
 }
