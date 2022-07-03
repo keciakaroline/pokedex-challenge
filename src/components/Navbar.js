11b4b05958046858fca331907f1f21c7a06f5bc8
Kécia Santos <keciakaroline@gmail.com>
2022-07-03 16:10:47 +0200

    added number of favs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography } from "@mui/material";
-
+import FavoriteContext from "../contexts/favoritesContext";
 import "./styles/Navbar.css";
 
 export default function Navbar() {
+  const { favoritePokemons } = useContext(FavoriteContext);
+
   return (
     <AppBar position="fixed">
-      <Toolbar className="AppBar">
+      <Toolbar
+        style={{ backgroundColor: "#c80000", width: "100%", height: "100%" }}
+      >
         <Link to="/" className="Link">
-          <Typography variant="h6" component="div" className="Typography">
+          <Typography
+            variant="h6"
+            component="div"
+            style={{
+              paddingLeft: "15px",
+            }}
+          >
             Pokedex
           </Typography>
         </Link>
         <Link to="/favorites" className="Link">
-          <Typography variant="h6" component="div" className="Typography">
-            Favorites
+          <Typography
+            variant="h6"
+            component="div"
+            style={{
+              paddingLeft: "15px",
+            }}
+          >
+            Favorites ({favoritePokemons.length})
           </Typography>
         </Link>
       </Toolbar>
