827ea069c1f911109456fd62197d0228acc2db30
Kécia Santos <keciakaroline@gmail.com>
2022-07-03 03:53:08 +0200

    Added page to each pokemon

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,36 +1,118 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Container } from "@mui/material";
+import { useParams } from "react-router-dom";
+import "./styles/PokemonDetails.css";
 
-export default function PokemonDetails({ pokemon, pokemons }) {
+export default function PokemonDetails() {
+  const { name } = useParams();
+  const [pokemonDetail, setPokemonDetail] = useState(null);
+
+  const getPokemonDetail = async (name) => {
+    try {
+      let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPokemonDetail(data);
+    } catch (error) {
+      console.log("fetchPokemonError: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getPokemonDetail(name);
+  }, []);
+
+  if (!pokemonDetail) return null;
+
   return (
-    <div style={{ marginTop: 100 }}>
-      teste
-      {pokemon.name}
-      <Container>
-        {/* <img width="100%" height="auto" src="/" alt="/" />
-
-        <Typography variant="h2">
-          {selectedPokemonDetails?.species.name}
+    <Container
+      style={{
+        marginTop: 100,
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        component="div"
+        variant="h6"
+        style={{
+          fontSize: "36px",
+          textTransform: "capitalize",
+          textDecoration: "underline",
+        }}
+      >
+        {pokemonDetail.name}
+      </Typography>
+      <img
+        width="350px"
+        height="auto"
+        src={pokemonDetail.sprites.front_default}
+        alt={pokemonDetail.name}
+      />
+      <div className="pokemon-details">
+        <Typography variant="h6" className="pokemon-details-info">
+          Type:
         </Typography>
-
-        <Typography>
-          {selectedPokemonDetails?.types
-            .map((type) => {
-              return type.type.name;
-            })
-            .join(", ")}
+        <Typography variant="h6" className="pokemon-details">
+          {pokemonDetail.types.map((type, index) => {
+            return <div key={index}>{type.type.name}</div>;
+          })}
         </Typography>
+      </div>
 
-        <div>{selectedPokemonDetails?.height}</div>
-        <div>{selectedPokemonDetails?.weight}</div>
-        <div>{selectedPokemonDetails?.base_experience}</div>
+      <div className="pokemon-details">
+        <Typography variant="h6" className="pokemon-details-info">
+          Height:
+        </Typography>
+        <Typography component="div" variant="h6" className="pokemon-details">
+          <div>{pokemonDetail.height}</div>
+        </Typography>
+      </div>
 
-        <div>
-          {selectedPokemonDetails?.abilities
-            .map((ability) => ability.ability.name)
-            .join(", ")}
-        </div> */}
-      </Container>
-    </div>
+      <div className="pokemon-details">
+        <Typography variant="h6" className="pokemon-details-info">
+          Weight:
+        </Typography>
+        <Typography component="div" variant="h6" className="pokemon-details">
+          <div>{pokemonDetail.weight}</div>
+        </Typography>
+      </div>
+
+      <div className="pokemon-details">
+        <Typography variant="h6" className="pokemon-details-info">
+          Base Exp:
+        </Typography>
+        <Typography component="div" variant="h6" className="pokemon-details">
+          <div>{pokemonDetail.base_experience}</div>
+        </Typography>
+      </div>
+
+      <div className="pokemon-details">
+        <Typography variant="h6" className="pokemon-details-info">
+          Abilities:
+        </Typography>
+        <Typography component="div" variant="h6" className="pokemon-details">
+          <div>
+            {pokemonDetail.abilities
+              .map((ability) => ability.ability.name)
+              .join(", ")}
+          </div>
+        </Typography>
+      </div>
+
+      <div className="pokemon-details-stats">
+        {pokemonDetail.stats &&
+          pokemonDetail.stats.map((stat, idx) => {
+            return (
+              <div key={idx}>
+                <div className="stats">{stat.stat.name}</div>
+                <div className="stats">{stat.base_stat}</div>
+              </div>
+            );
+          })}
+      </div>
+    </Container>
   );
 }
