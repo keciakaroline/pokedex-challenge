845b1a1845933d1c9cd25d9d0e362e21364f31df
Kécia Santos <keciakaroline@gmail.com>
2022-07-03 03:53:18 +0200

    Added link pro pokemon detail

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
+import PokemonDetails from "./PokemonDetails";
 
 import "./styles/PokemonCard.css";
 
 export default function PokemonCard({ pokemon, index }) {
-  const navigate = useNavigate();
-
-  function goToPokemonDetails() {
-    navigate(`/pokemon/${pokemon.name}`);
-  }
-
   return (
     <Grid item xs={12} sm={6} md={4} lg={2} key={index}>
-      {/* <Link to={`/pokemon/${pokemon.name}`} className="Link"> */}
       <Card className="Card">
         <CardContent className="CardContent">
           <Typography component="div" variant="h6" className="Typography">
@@ -22,15 +16,24 @@ export default function PokemonCard({ pokemon, index }) {
           <Typography component="div" variant="h6" className="Typography">
             {pokemon.name}
           </Typography>
+          {/* <Typography component="div" variant="h6" className="Typography">
+            {pokemon.types.map((type, index) => {
+              return <div key={index}>{type.type.name}</div>;
+            })}
+          </Typography> */}
         </CardContent>
-        <CardMedia
-          onClick={goToPokemonDetails}
-          image={pokemon.sprites.front_default}
-          className="CardMedia"
-          alt={pokemon.name}
-        ></CardMedia>
+        <Link
+          to={`/pokemon/${pokemon.name}`}
+          className="Link"
+          pokemon={pokemon}
+        >
+          <CardMedia
+            image={pokemon.sprites.front_default}
+            className="CardMedia"
+            alt={pokemon.name}
+          ></CardMedia>
+        </Link>
       </Card>
-      {/* </Link> */}
     </Grid>
   );
 }
