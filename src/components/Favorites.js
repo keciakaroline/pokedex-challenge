e73243b3794571855b07a96ba767a1475729d641
Kécia Santos <keciakaroline@gmail.com>
2022-07-03 16:12:09 +0200

    added favorite webpage

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Container, Grid } from "@mui/material";
+import FavoriteContext from "../contexts/favoritesContext";
+import PokemonCard from "./PokemonCard";
 
 export default function Favorites() {
-  return <div>Favorites</div>;
+  const { favoritePokemons } = useContext(FavoriteContext);
+
+  return (
+    <div
+      style={{
+        marginTop: "100px",
+        width: "100%",
+        height: "100%",
+        marginLeft: "10px",
+      }}
+    >
+      {favoritePokemons >= 0 ? (
+        <h1>You don't have any favorites...</h1>
+      ) : (
+        <Container>
+          <div>
+            <Grid container spacing={2}>
+              Favorites
+              {favoritePokemons &&
+                favoritePokemons.map((pokemon, index) => (
+                  <PokemonCard pokemon={pokemon} key={index} />
+                ))}
+            </Grid>
+          </div>
+        </Container>
+      )}
+    </div>
+  );
 }
